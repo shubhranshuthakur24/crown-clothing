7b4003f121fc1c5592499369de6968cd6e0f9a5c
Shubhranshu Thakur <shubhranshuthakur@Shubhranshus-MacBook-Air.local>
2021-08-04 08:20:52 +0530

    updating our app with a shop page, collection preview component and collection item component

--- src/App.js
+++ src/App.js
@@ -1,38 +1,15 @@
 import React from "react";
-import { Route, Link } from "react-router-dom";
+import { Route } from "react-router-dom";
 import "./App.css";
 
-const HomePage = (props) => {
-  return (
-    <div>
-      <button onclick={() => props.history.push("/topics")}>Topics</button>
-      <h1>HOME PAGE</h1>
-    </div>
-  );
-};
-
-const TopicsList = () => {
-  return (
-    <div>
-      <h1>TOPICS LIST PAGE </h1>
-    </div>
-  );
-};
-
-const TopicDetail = (props) => {
-  return (
-    <div>
-      <h1>TOPICS DETAIL PAGE: {props.match.params.topicId}</h1>
-    </div>
-  );
-};
+import HomePage from "./pages/homepage/homepage.components";
+import ShopPage from "./pages/shop/shop.componenet.jsx";
 
 function App() {
   return (
     <div>
       <Route exact path="/" component={HomePage} />
-      <Route exact path="/topics" component={TopicsList} />
-      <Route path="/topics/:topicId" component={TopicDetail} />
+      <Route path="/shop" component={ShopPage} />
     </div>
   );
 }
